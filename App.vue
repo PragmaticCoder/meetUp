d695107ecc9d5a8fa87aba1bf93a4f43ba08cb31
Alvi Akbar <alvi.akbar@realpythonengineer.com>
2020-04-16 06:25:35 -0600

    created our fast component

--- App.vue
+++ App.vue
@@ -1,11 +1,16 @@
 <template>
   <view class="container">
-    <text class="text-color-primary">{{ title }}</text>
+    <Hello />
   </view>
 </template>
 
 <script>
+import Hello from "./src/components/Hello";
+
 export default {
+  components: {
+    Hello
+  },
   data() {
     return {
       title: "SHHH Confessions"
@@ -20,8 +25,5 @@ export default {
   align-items: center;
   justify-content: center;
   flex: 1;
-}
-.text-color-primary {
-  color: blue;
 }
 </style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,19 @@
+<template>
+  <text class="text-color-primary">{{ message }}</text>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Welcome to SHHH Confessions"
+    };
+  }
+};
+</script>
+
+<style>
+.text-color-primary {
+  color: blue;
+}
+</style>
